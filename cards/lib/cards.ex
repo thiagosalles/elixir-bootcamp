de8159a47ed5113013a0dc4ebf251c0dc20f280c
Thiago Salles <tsmlima@gmail.com>
2021-06-28 22:51:25 -0300

    Creating the Cards.contains?/2 method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -24,8 +24,27 @@ defmodule Cards do
       iex> Cards.shuffle(["Ace", "Two", "Three"])
       ["Ace", "Three", "Two"]
 
+      iex> Cards.shuffle(["Ace", "Two", "Three"])
+      ["Three", "Two", "Ace"]
+
   """
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  @doc """
+  Check if a collection cards contains a specific card
+
+  ## Examples
+
+      iex> Cards.contains(["Ace", "Two", "Three"], "Two")
+      true
+
+      iex> Cards.contains(["Ace", "Two", "Three"], "Four")
+      false
+
+  """
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
