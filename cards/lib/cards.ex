eebfe20f24a4d7726cb12c8da4d986e86a73bba4
Thiago Salles <tsmlima@gmail.com>
2021-07-01 00:51:35 -0300

    Passing File.read result directly to case statement

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -104,11 +104,9 @@ defmodule Cards do
 
   """
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "The file does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file does not exist"
     end
   end
 end
