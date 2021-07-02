7fc0146b346189b99ffcb7b0377674b866b6c26d
Thiago Salles <tsmlima@gmail.com>
2021-07-02 01:37:33 -0300

    mix format

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,11 +1,13 @@
 defmodule CardsTest do
   use ExUnit.Case, async: true
-  doctest Cards, except: [
-    create_deck: 0,
-    create_hand: 1,
-    shuffle: 1,
-    deal: 2
-  ]
+
+  doctest Cards,
+    except: [
+      create_deck: 0,
+      create_hand: 1,
+      shuffle: 1,
+      deal: 2
+    ]
 
   describe "create_deck/1" do
     test "makes 20 cards" do
