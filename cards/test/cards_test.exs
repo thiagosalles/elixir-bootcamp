b3b66235a9dafbb0949e2be6f81679cfe7dece72
Thiago Salles <tsmlima@gmail.com>
2021-07-02 01:11:33 -0300

    Creating some test cases

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -6,4 +6,19 @@ defmodule CardsTest do
     shuffle: 1,
     deal: 2
   ]
+
+  describe "create_deck/1" do
+    test "makes 20 cards" do
+      deck = Cards.create_deck()
+      assert length(deck) == 20
+    end
+  end
+
+  describe "shuffle/1" do
+    test "randomize the deck" do
+      deck = Cards.create_deck()
+      shuffled = Cards.shuffle(deck)
+      assert shuffled != deck
+    end
+  end
 end
